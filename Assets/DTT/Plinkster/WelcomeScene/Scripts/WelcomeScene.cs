338aeeb84406db6ede6d1dfef0ced838101b174f
rodion3000 <102345066+rodion3000@users.noreply.github.com>
2025-02-21 12:29:42 +0500

    4

--- Assets/DTT/Plinkster/WelcomeScene/Scripts/WelcomeScene.cs
+++ Assets/DTT/Plinkster/WelcomeScene/Scripts/WelcomeScene.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class WelcomeScene : MonoBehaviour
 {
     private float loadingTime = 3f; // Время загрузки в секундах
     [SerializeField] private Image progressBar; // Ссылка на Image для заполнения прогресс-бара
+    [SerializeField] private TextMeshProUGUI loadingPercentageText; // Ссылка на текст для отображения процентов загрузки
 
     void Start()
     {
@@ -23,6 +25,7 @@ public class WelcomeScene : MonoBehaviour
             elapsedTime += Time.deltaTime;
             float progress = Mathf.Clamp01(elapsedTime / loadingTime);
             progressBar.fillAmount = progress; // Обновляем заполнение прогресс-бара
+            loadingPercentageText.text = (progress * 100).ToString("F0") + "%"; // Форматируем текст
             yield return null; // Ждем следующего кадра
         }
 
